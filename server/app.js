4d6c6cf8be8c8fbd778a299a7d42faefca3fc945
Hebert Porto <hebertporto@gmail.com>
2017-05-13 10:46:50 -0300

    separando projetos, instalando dependencias

---
+++ server/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var cors = require('cors');
+var colors = require('colors');
+
+
+var port = process.env.PORT || 3000;
+
+io.on('connection', function(socket) {
+  
+});
+
+server.listen(port, function() {
+    console.log('Server listening at port %d'.yellow, port);
+});
