7cabdbf0cf2bac53aed23ea9848af070371b53bc
Hebert Porto <hebertporto@gmail.com>
2017-05-14 13:32:17 -0300

    ajustando funcionalidades

--- server/app.js
+++ server/app.js
@@ -9,6 +9,12 @@ var colors = require('colors');
 var port = process.env.PORT || 3000;
 
 io.on('connection', function(socket) {
+  socket.on('message to server', function(data){
+    console.log('mensagem', data.msg);
+     socket.broadcast.emit('message to all', { msg: data.msg });
+     socket.emit('message from server', { msg: data.msg });
+     
+  });
   console.log('cliente conectou');
 });
 
